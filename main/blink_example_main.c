5eb28b3e56581dc38c0b71ab483c12d7e5ed6d2a
Yasin Yaman <y.yaman.bicycle@gmail.com>
2023-06-06 09:08:37 +0200

    Adds initial Poti Functions

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -1,11 +1,3 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -16,23 +8,35 @@
 
 static const char *TAG = "example";
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
 static uint8_t s_led_state = 0;
 
-#ifdef CONFIG_BLINK_LED_RMT
-
 static led_strip_handle_t led_strip;
+
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+static esp_adc_cal_characteristics_t adc1_chars;
+
+static void poti_init(void)
+{
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
+    adc1_config_channel_atten(ADC1_CHANNEL_2, ADC_ATTEN_DB_11);
+}
+
+static int poti_read(void)
+{
+    int voltageInput = adc1_get_raw(ADC1_CHANNEL_2);
+    return voltageInput;
+}
 
 static void blink_led(void)
 {
     /* If the addressable LED is enabled */
     if (s_led_state) {
         /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        led_strip_set_pixel(led_strip, 0, 255, 0, 0);
         /* Refresh the strip to send data */
         led_strip_refresh(led_strip);
     } else {
@@ -57,35 +61,20 @@ static void configure_led(void)
     led_strip_clear(led_strip);
 }
 
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#endif
-
 void app_main(void)
 {
 
     /* Configure the peripheral according to the LED type */
     configure_led();
+    poti_init();
 
     while (1) {
+        int readout = poti_read();
+        ESP_LOGI(TAG, "Poti Value is %d", readout);
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
