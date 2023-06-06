1c77619c3ed01a8ebf99c0db11025b6f4006a188
Yasin Yaman <y.yaman.bicycle@gmail.com>
2023-06-06 09:49:34 +0200

    Edits blink_led Function to switch colour depending on the Frequency

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -42,17 +42,66 @@ static int poti_read(void)
     return Frequency;
 }
 
-static void blink_led(void)
+static void blink_led(int frequency)
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 255, 0, 0);
-        /* Refresh the strip to send data */
+   /* do not turn the led off when frequency is o*/
+    if (frequency == 0)
+    {   /*white*/
+        led_strip_set_pixel(led_strip, 0, 255, 255, 255);
         led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
+        s_led_state = 1;
+    }
+    else
+    {
+        if (s_led_state)
+        {
+            /* changes colour depending on the frequency*/
+            switch (frequency)
+            {
+            case 1000: /*red*/
+                led_strip_set_pixel(led_strip, 0, 255, 0, 0);
+                break;
+            case 500: /*lime*/
+                led_strip_set_pixel(led_strip, 0, 0, 255, 0);
+                break;
+            case 333:/*blue*/
+                led_strip_set_pixel(led_strip, 0, 0, 0, 255);
+                break;
+            case 250:/*yellow*/
+                led_strip_set_pixel(led_strip, 0, 255, 255, 0);
+                break;
+            case 200:/*Magenta*/
+                led_strip_set_pixel(led_strip, 0, 255, 0, 255);
+                break;
+            case 166:/*Cyan*/
+                led_strip_set_pixel(led_strip, 0, 0, 255, 255);
+                break;
+            case 142:/*orange*/
+                led_strip_set_pixel(led_strip, 0, 255, 165, 0);
+                break;
+            case 125:/*dark green*/
+                led_strip_set_pixel(led_strip, 0, 0, 100, 0);
+                break;
+            case 111:/*dark blue*/
+                led_strip_set_pixel(led_strip, 0, 0, 0, 139);
+                break;
+            case 100:/*seashell*/
+                led_strip_set_pixel(led_strip, 0, 255, 245, 238);
+                break;
+            default:/*deep pink*/
+                led_strip_set_pixel(led_strip, 0, 255, 20, 147);
+                break;
+            }
+            /* Refresh the strip to send data */
+            led_strip_refresh(led_strip);
+        }
+        else
+        {
+            /* Set all LED off to clear all pixels */
+            led_strip_clear(led_strip);
+        }
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
     }
 }
 
@@ -83,9 +132,8 @@ void app_main(void)
         int readout = poti_read();
         ESP_LOGI(TAG, "Poti Value is %d", readout);
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
+        blink_led(readout);
         /* Toggle the LED state */
-        s_led_state = !s_led_state;
         vTaskDelay(readout / portTICK_PERIOD_MS);
     }
 }
